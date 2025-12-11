59cb2460d07b2f278a5c961863da5e117fa44560
Raghav67816 <kumaraghav079@gmail.com>
2025-12-11 13:44:42 +0530

    added button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,5 @@
-import { View, Text, TouchableOpacity, TouchableOpacityProps, GestureResponderEvent } from "react-native"
+import { useState } from "react"
+import {View, Text, TouchableOpacity, TouchableOpacityProps, GestureResponderEvent } from "react-native"
 
 type ButtonProps = {
     title: string,
@@ -8,9 +9,22 @@ type ButtonProps = {
 export default function CButton({
     title, onPress
 }: ButtonProps){
+
+    const [textStyle, setTextStyle] = useState("text-md text-white")
+
+    function onPressWrapped(color: string){
+        onPress?.call
+        setTextStyle('text-md text-'.concat(color))
+    }
+
     return (
-        <TouchableOpacity className={'bg-white'} onPress={onPress}>
-            <Text>{title}</Text>
+        <TouchableOpacity 
+            className={'flex-col border-2 border-blue-500 p-4 w-content active:bg-blue-400 rounded-md'}
+            style={{alignSelf: "flex-start"}}
+            onPress={() => {
+                onPressWrapped("white")
+            }}>
+            <Text className={textStyle}>{title}</Text>
         </TouchableOpacity>
     )
 }
